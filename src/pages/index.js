b565a97e9284aea9d38b9e90b045bc1d52907c8b
Ryo.T <shg18.sk1.hr10@gmail.com>
2020-10-20 16:52:40 +0900

    create Google Map React base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+// examples:
 import GoogleMap from '../components/GoogleMap';
-// import Marker from '../components/Marker';
 
-const getInfoWindowString = place => `
-    <div>
-      <div style="font-size: 16px;">
-        ${place.name}
-      </div>
-      <div style="font-size: 14px; color: green;">
-        ${place.description}
-      </div>
-    </div>`;
-
-const handleApiLoaded = (map, maps, places) => {
-    const markers = [];
-    const infowindows = [];
-
-    places.forEach(place => {
-        markers.push(
-            new maps.Marker({
-                position: {
-                    lat: place.geometry.location.lat,
-                    lng: place.geometry.location.lng,
-                },
-                map,
-            })
-        );
-
-        infowindows.push(
-            new maps.InfoWindow({
-                content: getInfoWindowString(place),
-            })
-        );
-    });
-
-    markers.forEach((marker, i) => {
-        marker.addListener('click', () => {
-            infowindows[i].open(map, marker);
-        });
-    });
-};
-
-export default function Home() {
-    // 基準点: 大阪市役所
-    const defaultSettings = {
-        center: {
-            lat: 34.4111,
-            lng: 135.3008,
-        },
-        zoom: 11
-    };
-
-    const [places, setPlaces] = useState();
+const Home = () => {
+    const [places, setPlaces] = useState([]);
 
     useEffect(() => {
         fetch('places.json')
@@ -67,11 +21,12 @@ export default function Home() {
     return (
         <div style={{ height: '70vh', width: '70vh' }}>
             <GoogleMap
-                defaultCenter={defaultSettings.center}
-                defaultZoom={defaultSettings.zoom}
-                yesIWantToUseGoogleMapApiInternals
-                onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps, places)}
-            />
+            defaultZoom={10}
+            defaultCenter={{ lat: 34.4111, lng: 135.3008}}
+        />
         </div>
+
     );
-}
+};
+
+export default Home;
