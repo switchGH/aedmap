61590fe9627c135967b30b93b3582e9dbd3ad484
Tanoue Ryo <shg18.sk1.hr10@gmail.com>
2020-10-26 14:19:13 +0900

    separate components

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import InfoWindow from './InfoWindow';
 
-const Wrapper = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 18px;
-    height: 18px;
-    background-color: #000;
-    border: 2px solid #fff;
-    border-radius: 100%;
-    user-select: none;
-    transform: translate(-50%, -50%);
-    cursor: ${props => (props.onClick ? 'pointer' : 'default')};
-    &:hover {
-        z-index: 1;
-    }
-`;
+const Marker = ({ show, place }) => {
+    const markerStyle = {
+        border: '1px solid white',
+        borderRadius: '50%',
+        height: 10,
+        width: 10,
+        backgroundColor: show ? 'red' : 'blue',
+        cursor: 'pointer',
+        zIndex: 10,
+    };
 
-const Marker = ({ text, onClick }) => <Wrapper alt={text} onClick={onClick} />;
-
-Marker.defaultProps = {
-    onClick: null,
+    return (
+        <>
+            <div style={markerStyle} />
+            {show && <InfoWindow place={place} />}
+        </>
+    );
 };
 
 Marker.propTypes = {
-    onClick: PropTypes.func,
-    text: PropTypes.string.isRequired,
+    show: PropTypes.bool.isRequired,
+    place: PropTypes.shape({
+        name: PropTypes.string,
+        formatted_address: PropTypes.string,
+        rating: PropTypes.number,
+        types: PropTypes.arrayOf(PropTypes.string),
+        price_level: PropTypes.number,
+        opening_hours: PropTypes.shape({
+            open_now: PropTypes.bool,
+        }),
+    }).isRequired,
 };
 
 export default Marker;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,67 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getPlaces } from '../api';
 import GoogleMap from '../components/GoogleMap';
-
-const InfoWindow = props => {
-    const { place } = props;
-    const infoWindowStyle = {
-        position: 'relative',
-        bottom: 150,
-        left: '-45px',
-        width: 220,
-        backgroundColor: 'white',
-        boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
-        padding: 10,
-        fontSize: 14,
-        zIndex: 100,
-    };
-
-    return (
-        <div style={infoWindowStyle}>
-            <div style={{ fontSize: 14 }}>{place.name}</div>
-            {/* <div style={{ fontSize: 14 }}>
-        <span style={{ color: 'grey' }}>
-          {place.rating}
-          {' '}
-        </span>
-        <span style={{ color: 'orange' }}>
-          {String.fromCharCode(9733).repeat(Math.floor(place.rating))}
-        </span>
-        <span style={{ color: 'lightgrey' }}>
-          {String.fromCharCode(9733).repeat(5 - Math.floor(place.rating))}
-        </span>
-      </div>
-      <div style={{ fontSize: 14, color: 'grey' }}>
-        {place.types[0]}
-      </div>
-      <div style={{ fontSize: 14, color: 'grey' }}>
-        {'$'.repeat(place.price_level)}
-      </div>
-      <div style={{ fontSize: 14, color: 'green' }}>
-        {place.opening_hours.open_now ? 'Open' : 'Closed'}
-      </div> */}
-        </div>
-    );
-};
-
-const Marker = ({ show, place }) => {
-    const markerStyle = {
-        border: '1px solid white',
-        borderRadius: '50%',
-        height: 10,
-        width: 10,
-        backgroundColor: show ? 'red' : 'blue',
-        cursor: 'pointer',
-        zIndex: 10,
-    };
-
-    return (
-        <>
-            <div style={markerStyle} />
-            {show && <InfoWindow place={place} />}
-        </>
-    );
-};
+import Marker from '../components/Marker';
 
 class Home extends Component {
     constructor(props) {
@@ -115,32 +55,5 @@ class Home extends Component {
         );
     }
 }
-
-InfoWindow.propTypes = {
-    place: PropTypes.shape({
-        name: PropTypes.string,
-        formatted_address: PropTypes.string,
-        rating: PropTypes.number,
-        types: PropTypes.arrayOf(PropTypes.string),
-        price_level: PropTypes.number,
-        opening_hours: PropTypes.shape({
-            open_now: PropTypes.bool,
-        }),
-    }).isRequired,
-};
-
-Marker.propTypes = {
-    show: PropTypes.bool.isRequired,
-    place: PropTypes.shape({
-        name: PropTypes.string,
-        formatted_address: PropTypes.string,
-        rating: PropTypes.number,
-        types: PropTypes.arrayOf(PropTypes.string),
-        price_level: PropTypes.number,
-        opening_hours: PropTypes.shape({
-            open_now: PropTypes.bool,
-        }),
-    }).isRequired,
-};
 
 export default Home;
